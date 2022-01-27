ba97c98b316dfa1951435e2198eb1e1ac23649af
zhangxuefeng <2367807010@qq.com>
2022-01-27 12:15:39 +0800

    style(skill):add g6

--- components/skills/index.js
+++ components/skills/index.js
@@ -1,46 +1,77 @@
 import styles from "./index.module.scss";
+import React, { useEffect, useLayoutEffect } from "react";
+import G6 from "@antv/g6";
 
-const Skills = () => {
+const Skills = ({ skillData }) => {
+  useLayoutEffect(() => {
+    const container = document.getElementById("container");
+    const width = container.scrollWidth;
+    const height = container.scrollHeight || 500;
+    const graph = new G6.TreeGraph({
+      container: "container",
+      width,
+      height,
+      linkCenter: true,
+      modes: {
+        default: [
+          {
+            type: "collapse-expand",
+            onChange: function onChange(item, collapsed) {
+              const data = item.get("model");
+              data.collapsed = collapsed;
+              return true;
+            },
+          },
+          "drag-canvas",
+          "zoom-canvas",
+        ],
+      },
+      defaultNode: {
+        size: 26,
+      },
+      layout: {
+        type: "compactBox",
+        direction: "RL",
+        getId: function getId(d) {
+          return d.id;
+        },
+        getHeight: () => {
+          return 26;
+        },
+        getWidth: () => {
+          return 26;
+        },
+        getVGap: () => {
+          return 20;
+        },
+        getHGap: () => {
+          return 30;
+        },
+        radial: true,
+      },
+    });
+
+    graph.node(function (node) {
+      return {
+        label: node.id,
+      };
+    });
+    graph.data(skillData);
+    graph.render();
+    graph.fitView();
+
+    if (typeof window !== "undefined")
+      window.onresize = () => {
+        if (!graph || graph.get("destroyed")) return;
+        if (!container || !container.scrollWidth || !container.scrollHeight)
+          return;
+        graph.changeSize(container.scrollWidth, container.scrollHeight);
+      };
+  });
   return (
     <div className={styles.wrap}>
       <div className={styles.title}>Skills</div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="350"
-        height="300"
-        viewBox="0 0 700 600"
-        className={styles.svg}
-      >
-        <line
-          x1="375"
-          y1="300"
-          x2="150"
-          y2="150"
-          style={{ stroke: "rgb(99,99,99)", strokeWidth: 2 }}
-        />
-        <text x="120" y="140" fill="black" className={styles.svgtext}>
-          前端工程化
-        </text>
-        <text x="330" y="280" fill="black" className={styles.svgtext}>
-          JavaScript
-        </text>
-        <circle
-          cx="150"
-          cy="150"
-          r="5"
-          stroke="black"
-          stroke-width="2"
-          fill="black"
-        />
-        <circle
-          cx="375"
-          cy="300"
-          r="10"
-          stroke="black"
-          stroke-width="2"
-          fill="black"
-        />
-      </svg>
+      <div id="container"></div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,10 @@ import styles from "./index.module.scss";
 import WorkList from "../components/workList/index";
 import Skills from "../components/skills/index";
 import Contact from "../components/contact/index";
+import { getSkillData } from "../lib/skill";
 import { Link, Element, animateScroll as scroll } from "react-scroll";
 
-export default function Home() {
+export default function Home({ skillData }) {
   return (
     <Layout home>
       <Head>
@@ -66,7 +67,7 @@ export default function Home() {
         </Element>
         <Element name="skills">
           <div className={`${styles.section} ${styles.skills}`}>
-            <Skills />
+            <Skills skillData={skillData} />
           </div>
         </Element>
         <Element name="contact">
@@ -78,3 +79,12 @@ export default function Home() {
     </Layout>
   );
 }
+
+export const getServerSideProps = (context) => {
+  const skillData = getSkillData();
+  return {
+    props: {
+      skillData,
+    },
+  };
+};
